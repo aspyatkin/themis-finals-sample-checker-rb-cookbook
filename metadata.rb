66537c80ab3fce18526cd47e69d47c4a9cc09917
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-09 22:36:12 +0400

    Use ssh-private-keys cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'themis-finals-service1-checker'
 description 'Installs and configures Themis Finals sample service1 checker'
-version '1.1.13'
+version '1.2.0'
 
 recipe 'themis-finals-service1-checker', 'Installs and configures Themis Finals sample service1 checker'
 depends 'latest-git', '~> 1.1.11'
@@ -9,3 +9,4 @@ depends 'rbenv', '1.7.1'
 depends 'supervisor', '~> 0.4.12'
 depends 'modern_nginx', '~> 1.3.0'
 depends 'ssh_known_hosts', '~> 2.0.0'
+depends 'ssh-private-keys', '~> 1.0.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,24 +16,9 @@ end
 url_repository = "https://github.com/#{node[id]['github_repository']}"
 
 if node.chef_environment.start_with? 'development'
-  ssh_data_bag_item = nil
-  begin
-    ssh_data_bag_item = data_bag_item('ssh', node.chef_environment)
-  rescue
-    ::Chef::Log.warn 'Check whether ssh data bag exists!'
-  end
-
-  ssh_key_map = \
-    if ssh_data_bag_item.nil?
-      {}
-    else
-      ssh_data_bag_item.to_hash.fetch 'keys', {}
-    end
-
-  unless ssh_key_map.empty?
-    url_repository = "git@github.com:#{node[id]['github_repository']}.git"
-    ssh_known_hosts_entry 'github.com'
-  end
+  ssh_private_key node[id]['user']
+  ssh_known_hosts_entry 'github.com'
+  url_repository = "git@github.com:#{node[id]['github_repository']}.git"
 end
 
 git2 node[id]['basedir'] do
